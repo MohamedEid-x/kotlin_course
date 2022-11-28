c11efe9025ea12934df296abd52b3efc1da1599a
Mohamed Eid <meid89817@gmail.com>
2022-11-28 10:25:47 +0200

    update

---
+++ untitled/src/main/kotlin/Function/Returned function.kt
@@ -0,0 +1,16 @@
+package Function
+
+fun main() {
+
+}
+// function without returned type
+
+fun greeting(){
+    println("Hello")
+}
+// function with returned type must contain return keyword
+
+fun greeting2():String //returned type
+{
+    return "Hello"
+}
