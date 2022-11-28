32abd7f73244e4ca0154cab6be8fab0e2512f7ae
Mohamed Eid <meid89817@gmail.com>
2022-11-28 10:37:12 +0200

    update

--- untitled/src/main/kotlin/Function/Returned function.kt
+++ untitled/src/main/kotlin/Function/Returned function.kt
@@ -2,15 +2,29 @@ package Function
 
 fun main() {
 
+    greeting()
+    println("------------------------")
+    val x=greeting2()
+    println(x)
+    println("------------------------")
+   val test= isPositive(-1)
+    println(test)
+
 }
 // function without returned type
 
 fun greeting(){
-    println("Hello")
+    println("Hello1")
 }
 // function with returned type must contain return keyword
 
 fun greeting2():String //returned type
 {
-    return "Hello"
+    return "Hello2"
+}
+
+// function with Parameter
+fun isPositive(ref:Int):Boolean{
+    return ref>=0
+
 }
