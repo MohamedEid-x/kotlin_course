69d05d7d57b740c2c592bdb3cbd6672d476bc99b
Mohamed Eid <meid89817@gmail.com>
2022-12-02 00:54:22 +0200

    update

--- untitled/src/main/kotlin/lamdaExepression/how to use lambda function as a function paramerter.kt
+++ untitled/src/main/kotlin/lamdaExepression/how to use lambda function as a function paramerter.kt
@@ -1,9 +1,11 @@
 package lamdaExepression
 
 fun main() {
+    //normal fun
 greeting("hello Mohamed")
+    println("__________________________________________________________________")
 
-
+// calling function that you used lambda fun as a parameter
     greeting2 {  // if you used  lambda as  a parameter you should use  the curly braces with fun instead of the parentheses
 
         println("message 1")
@@ -11,6 +13,15 @@ greeting("hello Mohamed")
         println("message 1")
         println("message 1")
     }
+    println("__________________________________________________________________")
+// pass argument to lambda fun
+reapeter(5){index-> //do this code
+    if (index<3){
+        println("Hello")
+    }else{
+        println("Bye")
+    }
+}
 
 
 }
@@ -34,4 +45,10 @@ fun greeting2(block:()->Unit){
     repeat(5){println("-----")}
     block()
     repeat(5){println("-----")}
+}
+
+
+
+fun reapeter(times:Int,message:(Int)->Unit){
+    repeat(times){xx->message(xx)}
 }
