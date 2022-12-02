25692663a8604575450032812761dbdbfd63feeb
Mohamed Eid <meid89817@gmail.com>
2022-12-02 23:28:16 +0200

    update

--- untitled/src/main/kotlin/collectionsInKotlin/Array.kt
+++ untitled/src/main/kotlin/collectionsInKotlin/Array.kt
@@ -7,19 +7,19 @@ fun main() {
     //you can use tha array of specific type like intArrayOf() This array that accept only intgars
     data.forEach { println(it) }
 println("_______________________________________________________________________________________")
-val users= arrayOf(
-    user("Mohamed","Eid"),
-    user("Ahmed","Said")
-)
-   // users.forEach { println(it) }
-    // also you can add any data to this array with plus() method
-   val updateUsers= users.plus(user("may","Mohamed"))
- //   updateUsers.forEach { println(it) }// to print data with updates
-   // edit item with its index
+    val users= arrayOf(
+        user("Mohamed","Eid"),
+        user("Ahmed","Said")
+    )
+    users.forEach { println(it) }
+// also you can add any data to this array with plus() method
+    val updateUsers= users.plus(user("may","Mohamed"))
+        updateUsers.forEach { println(it) }// to print data with updates
+// edit item with its index
     users[0]=user("Ali","Samir")
     val Edit=users[0]
-    println(Edit) //print update
-    users.forEach { println(it) } //print all data in the array with last update
+        println(Edit) //print update
+        users.forEach { println(it) } //print all data in the array with last update
 
 
 
