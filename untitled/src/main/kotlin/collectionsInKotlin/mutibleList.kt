cfdd4fe68bd82c655cc52f280529b3806f490bc0
Mohamed Eid <meid89817@gmail.com>
2022-12-04 08:37:17 +0200

    update

--- untitled/src/main/kotlin/collectionsInKotlin/mutibleList.kt
+++ untitled/src/main/kotlin/collectionsInKotlin/mutibleList.kt
@@ -4,13 +4,14 @@ fun main() {
     val data= mutableListOf(1,3,3,4,5,4,3)
         data.forEach{println(it)}
     println("================================")
-    data.add(30)
+    data.add(30) //add item to list
     data.forEach{println(it)}
-    data.add(2,30)
+    data.add(2,30) //add item with specific index
     data.forEach{println(it)}
-    data.remove(30)
+    data.remove(30)//remove mention item
     data.forEach{println(it)}
-    data.removeAt(2)
+    data.removeAt(2)// remove mention index
+
 
 
 
