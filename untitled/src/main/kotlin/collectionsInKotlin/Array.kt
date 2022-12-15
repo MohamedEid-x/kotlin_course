929a5bded1dfc9d805ff3ace2eb8e04efa0dca77
Mohamed Eid <meid89817@gmail.com>
2022-12-15 23:41:21 +0200

    Update Array list file

--- untitled/src/main/kotlin/collectionsInKotlin/Array.kt
+++ untitled/src/main/kotlin/collectionsInKotlin/Array.kt
@@ -15,23 +15,23 @@ fun main() {
     //you can use tha array of specific type like intArrayOf() This array that accept only integers
     val data2= intArrayOf(1,2,3,4,5,6) // array that contains Integers only you can use also double or long etc.
 println("_______________________________________________________________________________________")
-   // 2.How to use class with constructor inside the array
+    // 2.How to use class with constructor inside the array
 
     val users= arrayOf(
         user("Mohamed","Eid"),
         user("Ahmed","Said"))
     users.forEach { println(it) }
-    println("_______________________________________________________________________________________")
+println("_______________________________________________________________________________________")
 
     // 3.How to add a new items to the array
 
-// also you can add any data to this array with plus() method
+    // also you can add any data to this array with plus() method
     val updateUsers= users.plus(user("may","Mohamed"))
         updateUsers.forEach { println(it) }// to print data with updates
-    println("_______________________________________________________________________________________")
+println("_______________________________________________________________________________________")
 
     //4.How to update any item with its index
-// edit item with its index
+    // edit item with its index
     users[0]=user("Ali","Samir")
     val Edit=users[0]
         println(Edit) //print update
